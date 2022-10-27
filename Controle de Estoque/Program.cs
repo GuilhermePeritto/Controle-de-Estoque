37d0365c35306b62c80ff3873f56fef5b8e2835f
Guilherme Bortolotto Peritto <perittoguilherme@gmail.com>
2022-10-27 20:29:39 -0300

    dg

--- Controle de Estoque/Program.cs
+++ Controle de Estoque/Program.cs
@@ -13,8 +13,8 @@ namespace Controle_Estoque {
             string[] nome_cliente = new string[100];
             int qtd_usuarios = 0, qtd_produto=0;
 
-            while(qtd_produto<10) {
-                id_produto[qtd_produto] = qtd_produto + 1;
+            while(qtd_produto<=10) { //geracao de estoque
+                id_produto[qtd_produto+1] = qtd_produto + 1;
                 qtd_estoque[qtd_produto] = numAleatorio.Next(0, 100);
                 qtd_produto++;
             }
@@ -26,6 +26,11 @@ namespace Controle_Estoque {
                 opcao = int.Parse(Console.ReadLine());
                 switch (opcao) {
                     case 1:
+                        Console.Clear();
+
+                        for (int i = 1; i <= qtd_usuarios; i++) {
+                            Console.WriteLine("\n                                              Cod Cliente: " + id_cliente[i] + " Nome Cliente: " + nome_cliente[i]);
+                        }
                         Console.WriteLine("\nInforme o codigo do cliente: ");
                         cod_cliente = int.Parse(Console.ReadLine());
                         for (int i = 0; i < 10; i++) {
@@ -110,8 +115,14 @@ namespace Controle_Estoque {
                     case 5:
                         Console.Clear();
                         for (int i = 1; i <= qtd_produto; i++) {
-                            Console.WriteLine("\n                                              Cod Produto: " + id_produto[i] + " Estoque: " + qtd_estoque[i]);
+                            if (id_produto[i]>0) {
+                                Console.WriteLine("\n                                              Cod Produto: " + id_produto[i] + " Estoque: " + qtd_estoque[i]);
+                            }
                         }
+                        string saida;
+                        Console.WriteLine("\n\nPrecione qualquer tecla para voltar ao menu: ");
+                        saida = Console.ReadLine();
+                        Console.Clear();
                         break;
 
                     case 9:
